567e9e7c06f49c12fa547f8de14b4a7076eed813
Nadav Tasher <tashernadav@gmail.com>
2024-01-17 23:26:52 +0200

    Updated test

--- tests/test_token.py
+++ tests/test_token.py
@@ -5,4 +5,8 @@ from guardify import *
 
 def test_token():
     # Create a token with value
-    token = Token("0123", "Test", {"Test1": "Test2", "Test2": 0}, 10000, 10000, ["Test"])
+    Token("0123", "Test", {"Test1": "Test2", "Test2": 0}, 10000, 10000, ["Test"])
+
+    # Make sure raises for wrong types
+    with pytest.raises(TypeError):
+        Token(1, 2, {3: "Test2", "Test2": 0}, 10000, 10000, ["Test"])
